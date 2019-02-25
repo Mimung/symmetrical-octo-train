28336d04301cf865a60d7f20419a303f2ac6d7e9
Paul <paul.kraakmo@gmail.com>
2019-02-25 15:20:09 +0100

    Create Copy membership.ps1

---
+++ Powershell/Copy membership.ps1
@@ -0,0 +1,8 @@
+﻿# Account name that you want to copy AD membership from
+$username1 = "enternamehere"
+
+# Account name that you want to copy AD membership to, do not edit this line, you will be prompted to enter the username
+$username2  = Read-host "Enter username to copy to: "
+ 
+# copy-paste process. Get-ADuser membership     | then selecting membership                       | and add it to the second user
+get-ADuser -identity $username1 -properties memberof | select-object memberof -expandproperty memberof | Add-AdGroupMember -Members $username2
