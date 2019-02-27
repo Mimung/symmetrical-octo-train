240a6dc052881293c17410caf5a92c6aa677d200
Paul <paul.kraakmo@gmail.com>
2019-02-27 16:00:17 +0100

    Update List exporter.ps1

--- Powershell/List exporter.ps1
+++ Powershell/List exporter.ps1
@@ -1,7 +1,12 @@
-﻿$group = "AD groupname here"
-$path = "file path here"
-$filename = "filename + extension here"
+﻿#Enter AD group name here:
+$group = "Test AD group"
+#Filepath to where you want to store the file Exmaple \\127.0.0.1\MyFolder\ or C:\MyFolder
+$path = "\\127.0.0.1\MyFolder\"
+#Enter Filename
+$filename = "MyList"
+#File Extension
+$extension = ".csv"
 
 #Do not edit below this line
 
-Get-ADGroupMember -identity $gruppe | select Name,@{Name="Office";Expression={ Get-ADUser $_.SamAccountName -Properties Office | Select -ExpandProperty Office }} | Export-csv -Delimiter ";" -path "$($path)$($filename)" -NoTypeInformation
+Get-ADGroupMember -identity $gruppe | select Name,@{Name="Office";Expression={ Get-ADUser $_.SamAccountName -Properties Office | Select -ExpandProperty Office }} | Export-csv -Delimiter ";" -path "$($path)$($filename)$($extension)" -NoTypeInformation
