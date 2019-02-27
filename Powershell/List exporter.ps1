2b2b8e1b4c9b23afb678c3ee7456a6e24cb1a80b
Paul <paul.kraakmo@gmail.com>
2019-02-27 12:10:15 +0100

    Create List exporter.ps1

---
+++ Powershell/List exporter.ps1
@@ -0,0 +1,8 @@
+﻿$group = "AD groupname here"
+$path = "file path here"
+$filename = "filename + extension here"
+
+#Do not edit below this line
+$fullpath = "$($path)$($filename)"
+
+Get-ADGroupMember -identity $gruppe | select Name,@{Name="Office";Expression={ Get-ADUser $_.SamAccountName -Properties Office | Select -ExpandProperty Office }} | Export-csv -Delimiter ";" -path $fullpath -NoTypeInformation
