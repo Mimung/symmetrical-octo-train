25a17f2090b990f8f3df8978a6197961bd657de5
Paul <paul.kraakmo@gmail.com>
2019-02-27 12:50:01 +0100

    Update List exporter.ps1

--- Powershell/List exporter.ps1
+++ Powershell/List exporter.ps1
@@ -3,6 +3,5 @@ $path = "file path here"
 $filename = "filename + extension here"
 
 #Do not edit below this line
-$fullpath = "$($path)$($filename)"
 
-Get-ADGroupMember -identity $gruppe | select Name,@{Name="Office";Expression={ Get-ADUser $_.SamAccountName -Properties Office | Select -ExpandProperty Office }} | Export-csv -Delimiter ";" -path $fullpath -NoTypeInformation
+Get-ADGroupMember -identity $gruppe | select Name,@{Name="Office";Expression={ Get-ADUser $_.SamAccountName -Properties Office | Select -ExpandProperty Office }} | Export-csv -Delimiter ";" -path "$($path)$($filename)" -NoTypeInformation
