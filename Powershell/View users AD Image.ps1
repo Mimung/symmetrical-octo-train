4aebbdfdd3e9c2c0dc2b0f01e0e626dd9cb8a390
Paul <paul.kraakmo@gmail.com>
2019-09-16 14:44:51 +0200

    V 1.1
    Update the script, change the path where the images gets downloaded. Also added a Read-host so you do not need to edit the script. Just need to run it

--- Powershell/View users AD Image.ps1
+++ Powershell/View users AD Image.ps1
@@ -6,22 +6,31 @@ Author:  Paul A. Kråkmo
 Contact: paul.kraakmo@gmail.com
 Github:  github.com/Mimung
 
-Version: 1.0 - Script Created
+Version: 1.1 - Script Created
 Date created:  12 sept 2019
-Date Modified: 12 sept 2019
+Date Modified: 16 sept 2019
 
 Description:
 
 If a photo is set on the users AD-account, 
 this script will then download that image as a JPG-file so that you can see it
 
+1.1: Added minor edit, it will now create a new folder C:\Temp\AdPhotos
+and store the pictures here
+
+It is now possible to enter the name in the comand window after running
+
 #>
 
 Import-Module ActiveDirectory
+
+New-Item -Path "c:\" -Name "Temp" -ItemType "directory" -EA SilentlyContinue
+New-Item -Path "c:\Temp\" -Name "AdPhotos" -ItemType "directory" -EA SilentlyContinue
  
-$UserName = "Username"
+$UserName = Read-host "Enter username to get the picture from: "
 $date = Get-Date -format "dd-MM-yyyy HH:mm"
-$PicturePath = "C:\temp\$($UserName)$($date).jpg"
+$PicturePath = "C:\temp\AdPhotos\$($UserName)$($date).jpg"
  
 $user = Get-ADUser $UserName -Properties thumbnailPhoto
-$user.thumbnailPhoto | Set-Content $PicturePath -Encoding byte 
+$user.thumbnailPhoto | Set-Content $PicturePath -Encoding byte
+Write-Host "Downloading of $UserName AD-photo is complete, image is saved here: $Picturepath "
