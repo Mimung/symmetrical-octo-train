5f3c0cd1099bbaaaeffbce1e647802fb7a8042d1
Paul <paul.kraakmo@gmail.com>
2019-09-16 19:00:19 +0200

    Update Get users full name haxxx.ps1

--- Powershell/Get users full name haxxx.ps1
+++ Powershell/Get users full name haxxx.ps1
@@ -4,3 +4,5 @@ $username = $env:UserName
 $nameCreate = Get-WMIObject Win32_UserAccount | where caption -eq $user | select FullName | ft -hide | Out-File C:\Temp\Name.txt
 $name = Get-Content -Path "C:\Temp\Name.txt" | Select -Index 1
 Remove-Item -Path "C:\Temp\Name.txt"
+
+Write-Host Your name is $name
